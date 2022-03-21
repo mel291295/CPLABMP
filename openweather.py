56692c04e80fbcd60aef73aceb1f5593e1fb29db
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-03-21 16:36:14 +0100

    Update openweather.py

--- openweather.py
+++ openweather.py
@@ -9,19 +9,19 @@ location= st.text_input("Gimmi a city", "london")
 
 #check API documentation to see what structure of URL is needed to access the data
 #http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric'
 st.text(url)
 
 
 # Download the JSON data from OpenWeatherMap.org's API.
 response = requests.get(url)  
 # Uncomment to see the raw JSON text:
-#print(response.text)  
+st.text(response.text)  
 
 #Load JSON data into a Python variable.
 weatherData = json.loads(response.text)
 # Uncomment to see the raw JSON text:
-#print(weatherData) 
+st.text(weatherData) 
 #from pprint import pprint 
 
 maxtemp = weatherData["main"]["temp_max"]
