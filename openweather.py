daa5f1412c322aae33b6bce24be611e8c814550e
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-03-21 16:03:59 +0100

    Create openweather.py

---
+++ openweather.py
@@ -0,0 +1,80 @@
+import json, requests 
+
+#add your own APIkey
+APIkey = "# ! python3
+import json, requests 
+
+# add your own APIkey
+APIkey = # ! python3
+import json, requests 
+
+# add your own APIkey
+APIkey = "7b3f6e505b459240312ab53b78c72e8b"
+location = 'london'
+
+# check API documentation to see what structure of URL is needed to access the data
+# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
+# print(url)
+
+
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+# print(response.text)  
+
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+# print(weatherData) 
+# from pprint import pprint 
+# pprint(weatherData) 
+
+print(weatherData['main']['temp_max']) 
+# more???????????
+location = 'london'
+
+# check API documentation to see what structure of URL is needed to access the data
+# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
+# print(url)
+
+
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+# print(response.text)  
+
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+# print(weatherData) 
+# from pprint import pprint 
+# pprint(weatherData) 
+
+print(weatherData['main']['temp_max']) 
+# more???????????
+location = 'london'
+
+#check API documentation to see what structure of URL is needed to access the data
+#http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
+#print(url)
+
+
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+#print(response.text)  
+
+
+#Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+#print(weatherData) 
+#from pprint import pprint 
+#pprint(weatherData) 
+
+print(weatherData['main']['temp_max']) 
