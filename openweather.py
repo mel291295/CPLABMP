47f68ae4ae768c6b045078e6f1bd5173979c2d80
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-03-21 16:23:58 +0100

    Update openweather.py

--- openweather.py
+++ openweather.py
@@ -1,42 +1,8 @@
-
+import streamlit as st
 import json, requests 
 
 APIkey = "7b3f6e505b459240312ab53b78c72e8b"
-location = 'london'
-
-# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
-st.text(url)
-
-response = requests.get(url)  
-
-weatherData = json.loads(response.text)
-
-st.text (weatherData['main']['temp_max']) 
-location = 'london'
-
-# check API documentation to see what structure of URL is needed to access the data
-# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
-# print(url)
-
-
-# Download the JSON data from OpenWeatherMap.org's API.
-response = requests.get(url)  
-# Uncomment to see the raw JSON text:
-# print(response.text)  
-
-
-# Load JSON data into a Python variable.
-weatherData = json.loads(response.text)
-# Uncomment to see the raw JSON text:
-# print(weatherData) 
-# from pprint import pprint 
-# pprint(weatherData) 
-
-print(weatherData['main']['temp_max']) 
-# more???????????
-location = 'london'
+location= input("Gimmi a city")
 
 #check API documentation to see what structure of URL is needed to access the data
 #http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
@@ -49,12 +15,13 @@ response = requests.get(url)
 # Uncomment to see the raw JSON text:
 #print(response.text)  
 
-
 #Load JSON data into a Python variable.
 weatherData = json.loads(response.text)
 # Uncomment to see the raw JSON text:
 #print(weatherData) 
 #from pprint import pprint 
-#pprint(weatherData) 
 
-print(weatherData['main']['temp_max']) 
+maxtemp = weatherData["main"]["temp_max"]
+pprint(weatherData)  
+
+
