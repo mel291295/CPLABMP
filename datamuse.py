96421643de95976f75d9b0ed22fd418b59045c9b
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-03-28 16:37:01 +0200

    Update datamuse.py

--- datamuse.py
+++ datamuse.py
@@ -3,20 +3,15 @@ from pprint import pprint
 
 st.header("Datamuse")
 
-keyword=st.text_input('plz give me a keyword ')
-url1= 'https://www.datamuse.com/api/words?rel_syn=' + keyword + '&max=10'
-url2= 'https://www.datamuse.com/api/words?rel_ant=' + keyword + '&max=10'
+keyword = st.text_input('plz give me a keyword ')
 
-response1 = requests.get(url1)   
-response2 = requests.get(url2) 
+choice = st.selectbox("What you want to know? ", ("Synonyms", "Antonyms", "Sounds like", "Means like"))
+if choice == "Synonyms":
+  st.write("You selected synonyms of " + keyword)
+if choice == "Antonyms" :
+  st.write("You selected antonyms of " + keyword)
+if choice == "Sounds like":
+  st.write ("You selected sounds like of " + keyword)
+if choice ==("You selected synonyms of " + keyword)
+  st.write("You selected means like " + keyword)
 
-dataFromDatamuse1 = json.loads(response1.text) 
-dataFromDatamuse2 = json.loads(response2.text) 
-pprint(dataFromDatamuse1)
-pprint(dataFromDatamuse2)
-
-print("you give me the word ", keyword, ". Its synonym are as following: ")
-for eachentry in dataFromDatamuse1:
-  print("-", eachentry["word"])
-for eachentry in dataFromDatamuse2:
-  print("-", eachentry["word"])
