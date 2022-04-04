73e71327c20cbff31c78bbd509e4a2849b16588a
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-04-04 16:48:59 +0200

    Update googletrans.py

--- class05/googletrans.py
+++ class05/googletrans.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 translator = Translator() 
 
-word= st.text_input("Give me a word: ", "word") 
- 
+word= st.text_input("Give me a word: ", "") 
+if word !="":
 abc = translator.translate(word, dest="it")
 st.write("The translation is ", abc.text) 
